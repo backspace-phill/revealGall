da182d1626af9409639ee4e7ddf63fd339b841f7
Phillip <psusemiehl@gmail.com>
2024-07-05 19:27:36 +0200

    Made the GalleryPage a lot nicer

--- Handler.cs
+++ Handler.cs
@@ -9,11 +9,74 @@ public class Handler
         var htmlFiles = Directory.GetFiles(".", "*.html", SearchOption.AllDirectories);
         return htmlFiles.Length == 0
             ? Results.Extensions.Html("<h1>No HTML files found</h1>")
-            : Results.Extensions.Html($"""
-    <h1>Hello Root!</h1>
+    //         : Results.Extensions.Html($"""
+    // <h1>Hello Root!</h1>
+    // <br>
+    // {htmlFiles.Select((content) => $"<a href={content}>{content}</a><iframe src={content} ></iframe>").Aggregate((a, b) => a + "<br>" + b)}
+    // """);
+        : Results.Extensions.Html("""
+<!DOCTYPE html>
+<html lang=\"en\">
+<head>
+    <meta charset=\"UTF-8\">
+    <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">
+    <title>Gallery</title>
+</head>
+<style>
+    body {
+        font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+        background-color: #2F2F2F;
+        color: white;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        flex-direction: column;
+    }
+    button {
+        color:blueviolet;
+        background-color: #2F2F2F;
+        border: 2px solid blueviolet;
+        border-radius: 10px;
+        padding: 10px;
+        font-size: 20px;
+        cursor: pointer;
+        width: 100%;
+        margin-bottom: 15px;
+    }
+    iframe {
+        pointer-events: none;
+        border-color: blueviolet;
+        border-width: 5px;
+        border-radius: 10px;
+        padding: 10px;
+    }
+    div {
+        display: flex;
+        padding: 10px;
+        justify-content: center;
+        flex-direction: column;
+    }
+    .stay-big {
+        width: 100vw;
+    }
+</style>
+<body>
+    <h1>Gallery</h1>
     <br>
-    {htmlFiles.Select((content) => $"<a href={content}>{content}</a><iframe src={content} ></iframe>").Aggregate((a, b) => a + "<br>" + b)}
-    """);
+    <div class=\"stay-big\">
+""" + htmlFiles.Select((s) => $"""
+    <div>
+        <a href={s}>
+            <button>Open {s} </button>
+        </a>
+        <iframe src={s}></iframe>
+    </div>
+    """).Aggregate((a,b) => a+b) + 
+"""
+</div>
+</body>
+</html>
+""");
     }
 
     public static IResult HandleIndividualRoute(string route)
